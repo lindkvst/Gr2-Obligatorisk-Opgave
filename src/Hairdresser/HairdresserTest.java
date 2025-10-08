763b9aba3f937f3701c1b1dd0aabad4cc3b34ac0
Tamir <tamirhaldlevi@gmail.com>
2025-10-08 10:04:49 +0200

    1st main method draft

--- src/Hairdresser/HairdresserTest.java
+++ src/Hairdresser/HairdresserTest.java
@@ -1,4 +1,32 @@
 package Hairdresser;
 
 public class HairdresserTest {
+
+    public static void main(String[] args) {
+        ScannerHelper sh = new ScannerHelper();
+
+    }
+
+    public void mainMenuProgram() {
+        boolean isDone = false;
+
+        while(!isDone) {
+        printMainMenu();
+        sh.
+
+        }
+    }
+
+    public void printMainMenu() {
+        System.out.println();
+        System.out.println("""
+                 Velkommen til start menuen!
+                ******************************
+                Tryk 1 for at booke en ny tid.
+                Tryk 2 for at slette en tid.
+                Tryk 3 for at se ledige tider.
+                Tryk 4 
+                ******************************
+                """);
+    }
 }
