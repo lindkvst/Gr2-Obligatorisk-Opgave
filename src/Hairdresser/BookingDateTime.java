e46b8463b0d7ab9cae775d89079e3b9356d7653f
abfa23 <abfa0002@stud.ek.dk>
2025-10-08 10:43:29 +0200

    BookingDateTime v3

--- src/Hairdresser/BookingDateTime.java
+++ src/Hairdresser/BookingDateTime.java
@@ -1,16 +1,19 @@
 package Hairdresser;
+import java.time.LocalDateTime;
 
 public class BookingDateTime {
+    private LocalDateTime dateTimeValue;
     private boolean isAvailable;
     private boolean isBooked;
     private String customerName;
     private boolean isPaid;
 
-    public BookingDateTime(boolean isAvailable, boolean isBooked, String customerName, boolean isPaid) {
-        this.isAvailable = isAvailable;
-        this.isBooked = isBooked;
-        this.customerName = customerName;
-        this.isPaid = isPaid;
+    public BookingDateTime(int year, int month, int day, int hour, int min) {
+        this.dateTimeValue = LocalDateTime.of(year, month, day, hour, min);
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTimeValue;
     }
 
     public boolean getAvailability() {
@@ -27,6 +30,10 @@ public class BookingDateTime {
 
     public boolean getPaymentStatus() {
         return isPaid;
+    }
+
+    public void setDateTimeValue(LocalDateTime dateTimeValue) {
+        this.dateTimeValue = dateTimeValue;
     }
 
     public void setAvailability(boolean isAvailable) {
