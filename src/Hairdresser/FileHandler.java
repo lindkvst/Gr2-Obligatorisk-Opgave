4a470f58cb09eaa4a15776b5ffc2d58077b04bb7
Tamir <tamirhaldlevi@gmail.com>
2025-10-09 10:56:07 +0200

    Buffered writer metode

--- src/Hairdresser/FileHandler.java
+++ src/Hairdresser/FileHandler.java
@@ -1,5 +1,16 @@
 package Hairdresser;
 
-public class FileHandler {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class FileHandler {
+    public void writeFile(String fileInput) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("Bookings.csv"))) {
+            bw.write(fileInput);
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("Error writing file.");
+        }
+    }
 }
