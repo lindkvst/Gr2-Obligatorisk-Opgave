0c5c5755bc1b2557cd561090421b2df83ed7c8a9
lindkvst <lindkviste@gmail.com>
2025-10-21 12:50:28 +0200

    Nu også med HairCut Class

---
+++ src/Hairdresser/HairCut.java
@@ -0,0 +1,12 @@
+package Hairdresser;
+
+public class HairCut extends HairSalonSale {
+
+    public HairCut(String productName, ProductType productType, double price) {
+        super(productName, productType, price);
+
+    }
+
+    //ingen getters og setters, da HairCut kun har nedarvede attributter
+
+}
